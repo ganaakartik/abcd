17000d7e4bbc855effbe5454629c423c5fbb8be0
ganaakartik <ganaakartik@gmail.com>
2021-03-26 12:34:15 +0530

    added

---
+++ src/sample/Sample.java
@@ -0,0 +1,17 @@
+package sample;
+
+public class Sample {
+	public static void main(String[] args) {
+		
+		
+		int a =10, b= 20;
+
+		System.out.println(a);
+		System.out.println(b);
+		
+	}
+	
+	
+	
+
+}
